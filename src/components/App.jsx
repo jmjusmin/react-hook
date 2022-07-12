487b83627404fc5ae0a271660f822e7d751849fe
jmjusmin <80433220+jmjusmin@users.noreply.github.com>
2022-07-12 17:59:32 +0700

    Using state and hook Create a function to increase and decrease a counter.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const [count, setCount] = useState(0); //initial state
+  // console.log(state[0]);
+
+  // const [red, green, blue] = [178, 222, 39];
+  // console.log(green);
+
+  function increase() {
+    // count++;
+    setCount(count + 1);
+  }
+  function decrease() {
+    // count++;
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
